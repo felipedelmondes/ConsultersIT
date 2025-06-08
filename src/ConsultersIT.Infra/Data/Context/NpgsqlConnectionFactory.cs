504574d20d531329dcb8f080d5da3ba5325ef593
Luis Felipe Delmondes <luis.delmondes@outlook.com>
2025-06-08 10:10:58 -0300

    Update NpgsqlConnectionFactory.cs

--- src/ConsultersIT.Infra/Data/Context/NpgsqlConnectionFactory.cs
+++ src/ConsultersIT.Infra/Data/Context/NpgsqlConnectionFactory.cs
@@ -11,7 +11,7 @@ public class NpgsqlConnectionFactory : IDbConnectionFactory
     
     public NpgsqlConnectionFactory(IConfiguration config)
     {
-        _connectionString = config.GetConnectionString("PostgresDb");
+        _connectionString = config.GetConnectionString("DefaultConnection");
     }
     
     public IDbConnection CreateConnection() => new NpgsqlConnection(_connectionString);
